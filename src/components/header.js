0d9f70bbabbbbb9814616a9b05b6f8e74d4ae99a
diamond mahar <diamondmahar999@gmail.com>
2025-08-03 17:00:56 +0545

    header and footer comits

---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+import './header.css';
+
+function Header() {
+    return (
+        <header>
+            <div>
+              <nav>
+                <ul>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Contact</li>
+                </ul>
+              </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
